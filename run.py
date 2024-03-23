2a224f6231694e056e892000cb3a714c6bc295a3
peter <peter.vlacuha@pobox.sk>
2024-03-23 21:05:29 +0000

    trying to push into geithub from vscode

---
+++ run.py
@@ -0,0 +1,8 @@
+# Your code goes here.
+# You can delete these comments, but do not change the name of this file
+# Write your code to expect a terminal of 80 characters wide and 24 rows high
+
+
+# Beggining of the code for the project 3: BATTLESHIPS project
+from random import randint
+
