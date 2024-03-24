438643525a40eeca0cd62edd46667b6946e58c7b
peter <peter.vlacuha@pobox.sk>
2024-03-24 00:47:47 +0000

    Made class for python battleship

--- run.py
+++ run.py
@@ -6,3 +6,19 @@
 # Beggining of the code for the project 3: BATTLESHIPS project
 from random import randint
 
+class BattleshipGame:
+    def __init__(self):
+        self.player_name = ""
+        self.rows = 0
+        self.cols = 0
+        self.player_board = []
+        self.opponent_board = []
+        self.player_score = 0
+        self.opponent_score = 0
+        self.previous_guesses = set()
+
+    def create_board(self, rows, cols):
+        return [["O" for _ in range(cols)] for _ in range(rows)]
+    
+    def get_board_size(self):       
+        
